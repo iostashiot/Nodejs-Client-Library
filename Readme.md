f65d2103840fe3ed427b76ad65621409b60e9011
Aravind Vijayan <vsaravind007@gmail.com>
2016-06-07 17:11:48 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,59 @@
+IOStash Nodejs Client Library
+===================
+
+
+[![NPM](https://nodei.co/npm-dl/iostash.png)](https://nodei.co/npm/iostash/)
+
+
+Nodejs Client library for IOStash IoT PaaS. Supports realtime data subscription to Devices, Data Points, Location, Channels and Custom Data sent to devices.
+
+ **Installing**
+
+Use NPM 
+
+    npm install iostash
+
+ 
+ **How To Use**
+
+This library supports all realtime events supported by IOStash. Refer to API Docs on http://iostash.com for more information and guides about using this library.
+
+      //Require library
+      var iostash = require('iostash')
+      //Initialize
+      iostash.init('X-ACCESS-TOKEN_HERE')
+      //Attach listeners
+      iostash.subscribeDevice('DEVICE-ID_HERE', function (data) {
+      //Handle data here
+      });  
+
+**Example**
+
+This example utilises a public device defined in IOStash.
+
+    var iostash = require('iostash')
+    iostash.initPublic('5734997f8680bc62de000006')
+    iostash.subscribeDevice('5734997f8680bc62de000006', function (update) {
+    	console.log(update)
+    })
+
+**Methods**   
+   
+
+ - `init(x-access-token)` - Initialises the object with specified x-access-token.
+ - `initPublic(deviceId)` - Initialises the object with a public device's ID.
+ - `subscribeDevice(deviceId,cb())` - Subscribes to data changes from the specified device.
+ - `unsubscribeDevice(deviceId,cb())` - Unsubscribes from the specified device.
+ - `subscribeDataPoint(deviceId,datapoint,cb())` - Subscribes to specified datapoint of the specified device.
+ - `unsubscribeDataPoint(deviceId,datapoint,cb())` - Unsubscribes from the specified data point.
+ - `subscribeLocation(deviceId,cb())` - Subscribes to location data from the specified device.
+ - `subscribeActions(deviceId,cb())` - Subscribes to device trigger action of the specified device.
+ - `subscribeCustomData(deviceId,cb())` - Subscribes to custom data sent to the device.
+
+**Notes**
+
+- `cb()` refers to callbacks and they will be executed during updates. 
+- While using `initPublic()` method for initialization, channel functions are not available.
+- This library does not (yet) support data pushing to IOStash.
+
+For support or assistance, drop an email to support@iostash.com
---
+++ iostash.js
@@ -0,0 +1,141 @@
+/*
+IOStash IoT PaaS Platform Library for Nodejs
+Copyright (c) 2016 Aravind VS
+http://iostash.com
+*/
+var io = require('socket.io-client')
+
+var access_token = null
+var socket = null
+var isPublic = false
+
+module.exports = {
+  init: function (token) {
+    if (!token) {
+      console.log('[IOStash] accessToken not specified!')
+      return
+    }
+    access_token = token
+    socket = io('https://api.iostash.io:83', { query: 'accessToken=' + access_token,'reconnect': true, 'reconnection delay': 500,'max reconnection attempts': 10 })
+    socket.on('connect', function () {
+      console.log('[IOStash] Server connection succeeded')
+    })
+
+    if (socket) {
+      socket.on('autherror', function (data) {
+        console.log('[IOStash AUTH ERROR] ' + data.message)
+      })
+    }
+
+    socket.on('disconnect', function () {
+      socket.disconnect()
+      console.log('Connection terminated')
+    })
+
+  },
+
+  initPublic: function (devToken) {
+    if (!devToken) {
+      alert('deviceToken is required!')
+      return
+    }
+
+    device_token = devToken
+    var host = 'https://api.iostash.io:83'
+    socket = io(host, {query: 'deviceToken=' + device_token,'reconnect': true, 'reconnection delay': 500,'max reconnection attempts': 10 })
+    socket.on('connect', function () {
+      isPublic = true
+    })
+
+    if (socket) {
+      socket.on('autherror', function (data) {
+        alert(data.message)
+        console.log('[IOStash Auth Error] ' + data.message)
+      })
+    }
+
+    socket.on('disconnect', function () {
+      socket.disconnect()
+      console.log('[IOStash] Connection terminated')
+    })
+
+  },
+
+  subscribeDevice: function (deviceId, callback) {
+    socket.emit('subscribeDevice', deviceId)
+    socket.on('devicesubscriptionFailed' + deviceId, function (data) {
+      console.log(data)
+    })
+    socket.on('deviceUpdate' + deviceId, function (data) {
+      callback(data)
+    })
+  },
+
+  unsubscribeDevice: function (deviceId, callback) {
+    socket.emit('unsubscribeDevice', deviceId)
+    socket.on('deviceUnsubcribed/#' + socket.id, function (data) {
+      callback(data)
+    })
+  },
+
+  subscribeChannel: function (channelId, callback) {
+    if (isPublic) {
+      console.log('[IOStash] Only public devices can be subscribed to in Public Mode')
+      return
+    }
+    socket.emit('channelSubscribe', channelId)
+
+    socket.on('channelsubscriptionFailed' + channelId, function (data) {
+      console.log(data)
+    })
+
+    socket.on('channelUpdate' + channelId, function (data) {
+      callback(data)
+    })
+  },
+
+  unsubscribeChannel: function (channelId, callback) {
+    socket.emit('channelUnsubscribe', channelId)
+    socket.on('channelunsubcribed/#' + socket.id, function (data) {
+      callback(data)
+    })
+  },
+
+  subscribeDataPoint: function (deviceId, dataPointName, callback) {
+    socket.emit('subscribeDataPoint', {'deviceID': deviceId,'dataPoint': dataPointName})
+
+    socket.on('datapointsubscriptionFailed' + deviceId + dataPointName, function (data) {
+      console.log(data)
+    })
+
+    socket.on('dataPointUpdate' + deviceId + dataPointName, function (data) {
+      callback(data)
+    })
+  },
+
+  unsubscribeDataPoint: function (deviceId, dataPointName, callback) {
+    socket.emit('unsubscribeDataPoint', deviceId + dataPointName)
+
+    socket.on('dataPointUpdateUnsubscribed/#' + socket.id, function (data) {
+      callback(data)
+    })
+  },
+
+  subscribeLocation: function (deviceId, callback) {
+    socket.on('newlocationUpdate' + deviceId, function (data) {
+      callback(data)
+    })
+  },
+  subscribeActions: function (deviceId, callback) {
+    socket.on('action' + deviceId, function (data) {
+      callback(data)
+    })
+  },
+
+  subscribeCustomData: function (deviceId, callback) {
+    socket.on('publish' + deviceId, function (data) {
+      callback(data)
+    })
+  }
+
+}
